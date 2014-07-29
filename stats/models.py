07d74a2ee1c9e6e93944f9df2b5a3566ae8aa5a5
undefiened <undefiened@gmail.com>
2014-07-29 20:31:49 +0400

    created models

--- stats/models.py
+++ stats/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    site = models.CharField(max_length=100)
+
+class ProjectDetails(models.Model)
+    project = models.ForeignKey(Project)
+    info1 = models.TextField()
+    info2 = models.TextField()
+
+class Data(models.Model)
+    project = models.ForeignKey(Project)
+    field = models,CharField(max_length=100)
+
+class Stat(models.Model)
+    data = models.ForeignKey(Data)
+    price = models.FloatField()
